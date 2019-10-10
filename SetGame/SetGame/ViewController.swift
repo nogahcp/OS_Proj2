bd1ceb0a3cd1345b19d92329fc96618c4d81ab39
Nogah Melamed Cohen <nogah@cherrypick-consulting.com>
2019-10-10 12:44:14 +0300

    New Project

---
+++ SetGame/SetGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetGame
+//
+//  Created by Nogah Melamed Cohen on 10/10/2019.
+//  Copyright © 2019 Nogah Melamed Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
